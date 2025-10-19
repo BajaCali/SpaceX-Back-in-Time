9137c0bb16611559c85270f2f3cb679c5ce6af03
Michal Němec <michaelnemec47@gmail.com>
2025-10-19 10:35:30 +0200

    refactor: Clean Code

--- SpaceX Back In Time/SpaceX Back In Time/Auxiliary/UIComponents/HorizontalProgressView.swift
+++ SpaceX Back In Time/SpaceX Back In Time/Auxiliary/UIComponents/HorizontalProgressView.swift
@@ -1,7 +1,27 @@
 import SwiftUI
 
+// MARK: - Constants
+
+extension HorizontalProgressView {
+    enum Constants {
+        static let defaultSpeed: Double = 1.3
+        static let defaultWidth: CGFloat = 100
+    }
+}
+
+// MARK: - Struct
+
 struct HorizontalProgressView: View {
+    let speed: Double
+    let width: CGFloat
+
     @State private var startDate = Date.now
+
+    init(speed: Double? = nil, width: CGFloat? = nil) {
+        self.speed = speed ?? Constants.defaultSpeed
+        self.width = width ?? Constants.defaultWidth
+        self.startDate = startDate
+    }
 
     var body: some View {
         TimelineView(.animation) { timeline in
@@ -9,12 +29,12 @@ struct HorizontalProgressView: View {
             let progress = getProgress(elapsedTime)
 
             ProgressView(value: progress)
-                .padding(.horizontal, 150)
+                .frame(width: width)
         }
     }
 
     func getProgress(_ timeElapsed: TimeInterval) -> Double {
-        let time = 1.3 * timeElapsed
+        let time = speed * timeElapsed
         return if Int(time) % 2 == 0 {
             (1 - fmod(Double(time), 1))
         } else {
