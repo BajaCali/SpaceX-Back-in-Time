846824068100a1a8f35805a99e5edf7c3f56efb2
Michal Němec <michaelnemec47@gmail.com>
2025-10-20 12:12:09 +0200

    ui: Update row view to match the domain

--- SpaceX Back In Time/SpaceX Back In Time/List/RowViews/RowView.swift
+++ SpaceX Back In Time/SpaceX Back In Time/List/RowViews/RowView.swift
@@ -3,15 +3,57 @@ import SwiftUI
 struct RowView: View {
     let launch: Launch
 
-    var singleLineText: String {
-        return if launch.success {
-            "⭐️ \(launch.title)"
-        } else {
-            launch.title
+    var body: some View {
+        HStack(spacing: 16) {
+            patch
+            titleAndDate
+            Spacer()
+            infoIcons
+        }
+        .padding(.vertical, 8)
+    }
+
+    var patch: some View {
+        Group {
+            if let patchURL = launch.patch?.small {
+                AsyncImage(url: patchURL) { image in
+                    image.resizable()
+                        .transition(.scale)
+                } placeholder: {
+                    ProgressView()
+                }
+            } else {
+                Image(Resources.Image.spaceXIcon)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+        }
+        .frame(width: 48, height: 48)
+    }
+
+    var titleAndDate: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(launch.title)
+                .font(.headline)
+            Text(launch.date, style: .date)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
         }
     }
 
-    var body: some View {
-        Text(singleLineText)
+    @ViewBuilder
+    var infoIcons: some View {
+        if !(launch.flickr?.original.isEmpty ?? true) {
+            Image(systemName: "photo.on.rectangle.angled")
+        }
+
+        if !launch.success {
+            Text("💥")
+        }
     }
+}
+
+#Preview {
+    RowView(launch: .withImages)
 }
